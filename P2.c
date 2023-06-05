b8bee23def7908a30af65e29d058a855b2105704
Igor-RC <128201635+Igor-RC@users.noreply.github.com>
2023-06-05 15:35:47 -0300

    Create P2.c

---
+++ P2.c
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int R, C, i;
+    int V[201], W[201], P[201];
+    scanf("&d", R);
+    scanf("&d", C);
+    if(R>2 && R<61){
+        if(C>=R && C<201){
+            for(i=0; i<C+1; i++){
+                scanf("&d", V[i]);
+                scanf("&d", W[i]);
+                scanf("&d", P[i]);
+                if(V[]>0 && W[]>0){
+                    if(V>=R && W>=R){
+                        if(P>0 && P<1001){
+                        
+                        }
+                    }
+                }
+                
+        }
+    }
+    return 0;
+}
